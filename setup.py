92be0ca5b132489d870d68cead5e9facd0aebab4
amit-nile <amit.sharma@niletechnologies.com>
2025-12-12 21:53:08 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Amit sharma",
+    author_email="amitnehwal.bkbiet@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
